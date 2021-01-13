8ac114981e9c401744ed5c1b09babe9cb76a3a22
Madhu Balaji <madhub2303@gmail.com>
2021-01-13 21:33:13 +0800

    fixed Procfile issue

--- newsbot.py
+++ newsbot.py
@@ -101,5 +101,5 @@ def index():
 if __name__ == '__main__':
     # note the threaded arg which allow
     # your app to have more than one thread
-    app.run(threaded=True)
+    newsbot.run(threaded=True)
 
